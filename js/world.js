9bb314c406f8b02678d5f09f0dd617da35bf5a69
lokrainmyth <f2kyy28kts@privaterelay.appleid.com>
2026-06-30 04:29:13 +0700

    Update world.js

--- js/world.js
+++ js/world.js
@@ -1,9 +1,9 @@
 class IoWorld {
-  constructor(player, storage) {
+  constructor(player) {
     this.player = player;
-    this.storage = storage;
-
     this.playlist = document.getElementById("playlist");
+
+    this.letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   }
 
   init(tracks) {
@@ -16,17 +16,39 @@ class IoWorld {
 
     this.tracks.forEach((t, i) => {
       const el = document.createElement("div");
-      el.className = "track locked";
+      el.className = "track";
+      el.dataset.index = i;
 
-      el.innerHTML = `•••`;
+      el.textContent = this.mask(t.title, i);
 
-      el.onclick = () => this.click(i, el);
+      el.onclick = () => this.play(i, el);
 
       this.playlist.appendChild(el);
+
+      this.glitch(el, i);
     });
   }
 
-  click(i, el) {
+  mask(text, seed) {
+    if (seed === 0) return text;
+
+    return text
+      .split("")
+      .map(c => (Math.random() > 0.7 ? c : this.letters[Math.floor(Math.random()*this.letters.length)]))
+      .join("");
+  }
+
+  glitch(el, i) {
+    if (i === 0) return;
+
+    setInterval(() => {
+      if (this.player.currentIndex >= i) return;
+
+      el.textContent = this.mask(this.tracks[i].title, i);
+    }, 120);
+  }
+
+  play(i, el) {
     if (i > this.player.currentIndex) return;
 
     this.player.currentIndex = i;
@@ -34,9 +56,6 @@ class IoWorld {
     this.player.play();
 
     el.classList.add("revealed");
-    el.innerHTML = `${i + 1}. ${this.tracks[i].title}`;
-
-    this.storage.saveIndex(i);
-    this.storage.saveCompleted(i);
+    el.textContent = this.tracks[i].title;
   }
 }
