70429b5847ab1e19f6c2f0afeb571eb3fac1c86c
lokrainmyth <f2kyy28kts@privaterelay.appleid.com>
2026-06-30 04:35:36 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,14 +3,15 @@ class IoApp {
     const res = await fetch("./data/tracks.json");
     const tracks = await res.json();
 
+    this.storage = new IoStorage();
     this.player = new IoPlayer();
-    this.world = new IoWorld(this.player);
+    this.world = new IoWorld(this.player, this.storage);
 
     this.player.load(tracks);
     this.world.init(tracks);
   }
 }
 
-window.onload = () => {
+window.addEventListener("DOMContentLoaded", () => {
   new IoApp().init();
-};
+});
